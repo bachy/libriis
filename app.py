6df9e6e907c42f391c70d9d1616e8854985c0d04
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2017-05-23 18:20:01 +0200

    display index.html through webserver with qtwebkit

--- app.py
+++ app.py
@@ -14,26 +14,35 @@ import sys
 from socket import socket
 import socketserver
 import http.server
+import threading
 
-# from PyQt5.QtWebKit import QWebView
-# from PyQt5.QtGui import QApplication
-# from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QFile, QIODevice, Qt, QTextStream, QUrl
+from PyQt5.QtWidgets import (QAction, QApplication, QLineEdit, QMainWindow, QSizePolicy, QStyle, QTextEdit)
+from PyQt5.QtNetwork import QNetworkProxyFactory, QNetworkRequest
+from PyQt5.QtWebKitWidgets import QWebPage, QWebView
 
-# web werver
+# find free port
 sock = socket()
 sock.bind(('', 0))
 PORT = sock.getsockname()[1]
-print(PORT)
 sock.close()
 # PORT = 8000
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
+# web werver
+# Handler = http.server.SimpleHTTPRequestHandler
+# httpd = socketserver.TCPServer(("", PORT), Handler)
+# httpd.serve_forever()
+httpd = http.server.HTTPServer(('', PORT), http.server.SimpleHTTPRequestHandler)
+thread = threading.Thread(target=httpd.serve_forever)
+thread.start()
 print("serving at port", PORT)
-httpd.serve_forever()
-# QT webkit
-# app = QApplication(sys.argv)
-# browser = QWebView()
-# browser.load(index.html)
-# browser.show()
 
-# app.exec_()
+
+# QT webkit
+app = QApplication(sys.argv)
+
+browser = QWebView()
+# browser.settings().setAttribute(QWebSettings.PluginsEnabled, True)
+browser.load(QUrl('http://localhost:'+str(PORT)))
+browser.show()
+
+sys.exit(app.exec_())
